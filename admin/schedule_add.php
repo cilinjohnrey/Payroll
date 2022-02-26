744b6d3a8740deba4826ade82ec6530cd2a02a59
cilinjohnrey <cilinjohnrey@gmail.com>
2022-02-26 11:30:16 +0800

    init

--- admin/schedule_add.php
+++ admin/schedule_add.php
@@ -3,9 +3,9 @@
 
 	if(isset($_POST['add'])){
 		$time_in = $_POST['time_in'];
-		$time_in = date('H:i:s', strtotime($time_in));
+		$time_in = date('h:i A', strtotime($time_in));
 		$time_out = $_POST['time_out'];
-		$time_out = date('H:i:s', strtotime($time_out));
+		$time_out = date('h:i A', strtotime($time_out));
 
 		$sql = "INSERT INTO schedules (time_in, time_out) VALUES ('$time_in', '$time_out')";
 		if($conn->query($sql)){
